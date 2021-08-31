5a8a2ee79fc1f90e13431709d0d3c0783b1530fd
myhub-ljc <2321170474@qq.com>
2021-08-31 20:46:49 +0800

    feat: 路径别名+vue-router+vuex的基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="app">
+    <h2>{{$store.state.name}}</h2>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore } from "vuex"
+
+const store = createStore({
+  state: () => {
+  return {
+      name: "kobe"
+    }
+  }
+})
+
+export default store;
