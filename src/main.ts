e650cdd73915c8a0d91af3d2d785fd91f81bbf4f
myhub-ljc <2321170474@qq.com>
2021-09-01 19:03:50 +0800

    feat: 按需引入element-plus并进行封装使用

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'element-plus/dist/index.css'
+
 import router from './router/index'
-import store from  "./store/index"
+import store from './store/index'
+import { globalRegister } from './global'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(globalRegister)
+
 app.mount('#app')
